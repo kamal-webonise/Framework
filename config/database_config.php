f6a3a51ba986f7246713472f6bc59e28abf00213
Vaibhav Tapadiya <vaibhav.tapadiya@weboniselab.com>
2018-12-04 16:31:01 +0530

    database_config.php file created

---
+++ config/database_config.php
@@ -0,0 +1,15 @@
+<?php
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+  'dsn' => '',
+  'hostname' => 'localhost',
+  'username' => '',
+  'password' => '',
+  'database' => '',
+  'dbdriver' => 'mysqli',
+  'pconnect' => FALSE,
+  'save_queries' => TRUE
+);
