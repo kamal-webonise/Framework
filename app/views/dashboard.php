53d73a8e3da38a28fb4c7be6eeb7e074704060a1
webonise <chirag.painter@weboniselab.com>
2018-12-14 15:12:59 +0530

    dashboard fixed

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -2,8 +2,8 @@
 $this->start('body'); ?>
 <h1>WELCOME TO DASHBOARD</h1>
 <h1><?php
-echo "email: ". ($this->dataArray[0]->email);
-echo "Full Name: ". ($this->dataArray[0]->name);
+echo "email: ". ($_SESSION['email']);
+echo "Full Name: ". ($_SESSION['name']);
 ?><h1>
 <a href = "../login/logout">Logout</a>
 <a href = "../user/deleteAccount">Delete Account</a>
